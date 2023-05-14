d2815f4045c94538da2ff7c11177c4cbc7a3e848
Ivana Skýva <ivana.rybakova@gmail.com>
2023-05-14 11:40:30 +0200

    rozdeleni emailu

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 console.log('funguju!');
 
+ const unreadPost = () => {
 fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails') 
 .then((response) => {
     return response.json();
@@ -7,8 +8,11 @@ fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails')
 .then((data) => {
     console.log(data.emails);
 
-    const listEmail = document.querySelector('#app');
-    listEmail.innerHTML = data.emails.map((props) => `
+    const listEmail = document.querySelector('#inbox');
+    listEmail.innerHTML = 
+    data.emails
+    .filter((props) => props.unread === true)
+    .map((props) => `
     <div class="email">
     <div class="email__head">
       <button class="email__icon email__icon--closed"></button>
@@ -22,6 +26,48 @@ fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails')
   </div>
     `,
     )
-     .join('');
-});
+    .join('');
+
+}); 
+}
+unreadPost(); 
+
+
+const readPost = () => {
+  fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails') 
+  .then((response) => {
+      return response.json();
+  })
+  .then((data) => {
+      console.log(data.emails);
+  
+      const listEmail2 = document.querySelector('#inbox2');
+      listEmail2.innerHTML += 
+      data.emails
+      .filter((props) => props.unread === false)
+      .map((props) => `
+      <div class="email">
+      <div class="email__head">
+        <button class="email__icon email__icon--opened"></button>
+        <div class="email__info">
+          <div class="email__sender">${props.sender.name}</div>
+          <div class="email__subject">${props.subject}</div>
+        </div>
+        <div class="email__time">${props.time}</div>
+      </div>
+      <div class="email__body"></div>
+    </div>
+      `,
+      )
+      .join('');
+  
+  }); 
+  }
+  readPost();  
+ 
+ 
+
+
+ 
+
 
