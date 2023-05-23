dd0d1620083d8140e855fa899bd77e35560c36c4
Ivana Skýva <ivana.rybakova@gmail.com>
2023-05-23 19:47:38 +0200

    uprava renderEmailList

--- script.js
+++ script.js
@@ -1,107 +1,59 @@
 console.log('funguju!');
 
-
-const ListEmailu = (items) => { 
-  return `
+const renderEmail = (item, isOpened) => {
+  const iconClass = isOpened ? 'email__icon--opened' : 'email__icon--closed';
   
-  <div class="email">
-  <div class="email__head">
-    <button class="email__icon email__icon--closed"></button>
-    <div class="email__info">
-      <div class="email__sender">${items.sender.name}</div>
-      <div class="email__subject">${items.subject}</div>
-    </div>
-    <div class="email__time">${items.time}</div>
-  </div>
-  <div class="email__body"></div>
-  </div>
-  `;
-  };
-
-  
-  
-   const renderEmailList = (props) => {
-    const emailList = document.querySelector('#inbox');
-    emailList.innerHTML +=
-    
-    props.map((item) => ListEmailu(item))
-    .join(''); 
-  };
-
-  const ListEmailu2 = (items) => { 
-    return `
-    
+ let emailHTML = `
+ 
     <div class="email">
-    <div class="email__head">
-      <button class="email__icon email__icon--opened"></button>
-      <div class="email__info">
-        <div class="email__sender">${items.sender.name}</div>
-        <div class="email__subject">${items.subject}</div>
+      <div class="email__head">
+        <button class="email__icon ${iconClass}"></button>
+        <div class="email__info">
+          <div class="email__sender">${item.sender.name}</div>
+          <div class="email__subject">${item.subject}</div>
+        </div>
+        <div class="email__time">${item.time}</div>
       </div>
-      <div class="email__time">${items.time}</div>
+      <div class="email__body"></div>
     </div>
-    <div class="email__body"></div>
-    </div>
-    `;
-    };
+  `;
+  
+  
+  return emailHTML;
+};
 
-  const renderEmailList2 = (props) => {
-    const emailList = document.querySelector('#inbox2');
-    emailList.innerHTML +=
+const renderEmailList = (props) => {
+  const emailList = document.querySelector('#inbox');
+  
+  props.forEach((item) => {
+    const isOpened = !item.unread;
+    const emailHTML = renderEmail(item, isOpened);
+    const emailElement = document.createElement('div');
     
-    props.map((item) => ListEmailu2(item))
-    .join(''); 
-  };
+    emailElement.classList.add('email');
+    if (item.unread) {
+      emailElement.classList.add('email--unread');
+    } else {
+      emailElement.classList.add('email--read');
+    }
+    
+    emailElement.innerHTML = emailHTML;
+    emailList.appendChild(emailElement);
+  }); 
+};
 
-
-  fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails')
+fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails')
   .then((response) => response.json())
   .then((data) => {
-    const filteredData = data.emails.filter((item) => item.unread === true);
-    renderEmailList(filteredData);
-    const filteredData2 = data.emails.filter((item) => item.unread === false);
-    renderEmailList2(filteredData2);
-  });
-  
-
-
-
-/*   
-const ListEmailu2 = (items) => { 
-  return `
-  
-  <div class="email">
-  <div class="email__head">
-    <button class="email__icon email__icon--opened"></button>
-    <div class="email__info">
-      <div class="email__sender">${items.sender.name}</div>
-      <div class="email__subject">${items.subject}</div>
-    </div>
-    <div class="email__time">${items.time}</div>
-  </div>
-  <div class="email__body"></div>
-  </div>
-  `;
-  };
-  
-   const renderEmailList2 = (props) => {
-  
-    const emailList2 = document.querySelector('#inbox2');
-    emailList2.innerHTML += 
-    props.map((item) => ListEmailu2(item))
-    .join('');
     
-  };
-  
-  
-  fetch('https://apps.kodim.cz/daweb/trening-api/apis/emails')
-  .then((response) => response.json())
-  .then((data) =>  {
-     const filteredData = data.emails.filter((item) => item.unread === false);
-  renderEmailList2(filteredData);
-});
-  
-   */
+    const unreadEmails = data.emails.filter((item) => item.unread === true);
+    renderEmailList(unreadEmails);
+    const readEmails = data.emails.filter((item) => item.unread === false);
+    renderEmailList(readEmails);
+  });
+
+
+
   
   
 
